32906c866dd33cf17fd74856fe4438f883a91b15
Jerish Bovas <jerishbradlyb@gmail.com>
2022-06-30 00:06:02 -0400

    filename test

--- ScavengerHunt/Services/BlobService.cs
+++ ScavengerHunt/Services/BlobService.cs
@@ -15,9 +15,9 @@ public class BlobService : IBlobService
         var blobContainer = blobServiceClient.GetBlobContainerClient(container);
         await blobContainer.CreateIfNotExistsAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
 
-        string ext = file.FileName.Split(".").Last();
+        string ext = file.FileName.Split(".").LastOrDefault() ?? "";
 
-        var blobClient = blobContainer.GetBlobClient($"{id}.{ext}");
+        var blobClient = blobContainer.GetBlobClient($"{id}{ext}");
 
         await blobClient.UploadAsync(file.OpenReadStream(), true);
 
