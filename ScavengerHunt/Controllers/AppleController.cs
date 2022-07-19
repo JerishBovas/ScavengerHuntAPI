5dc0a7eee61e2cd04360375d13a179747c067d5b
Jerish Bovas <jerishbradlyb@gmail.com>
2022-07-18 23:46:31 -0400

    fixed site association issue using new controller

---
+++ ScavengerHunt/Controllers/AppleController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScavengerHunt.Controllers {
+    [Route(".well-known/apple-app-site-association")]
+    public class AppleController : Controller {
+        private IHostEnvironment _hostingEnvironment;
+
+        public AppleController(IHostEnvironment environment) {
+            _hostingEnvironment = environment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() {
+            return Content(
+                await System.IO.File.ReadAllTextAsync(Path.Combine(_hostingEnvironment.ContentRootPath, "wwwroot/.well-known/apple-app-site-association")),
+                "text/plain"
+            );
+        }
+    }
+}
