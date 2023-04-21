c1a270c19238454c4459f849ec5dd8eebeb900d6
Jerish Bovas <jerishbradlyb@gmail.com>
2023-04-21 14:05:35 +0530

    additional properties added to GameDto

--- ScavengerHunt/DTOs/Game/GameDetailDto.cs
+++ ScavengerHunt/DTOs/Game/GameDetailDto.cs
@@ -1,20 +1,22 @@
-﻿using System;
-namespace ScavengerHunt.DTOs
+﻿namespace ScavengerHunt.DTOs
 {
 	public record struct GameDetailDto
 	{
         public Guid Id { get; set; }
         public bool IsPrivate { get; set; }
+        public bool IsReadyToPlay { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+        public Guid UserId { get; set; }
         public CoordinateDto Coordinate { get; set; }
         public ICollection<ItemDto> Items { get; set; }
         public string ImageName { get; set; }
         public int Difficulty { get; set; }
         public Double Ratings { get; set; }
         public List<string> Tags { get; set; }
+        public int GameDuration { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset LastUpdated { get; set; }
     }
--- ScavengerHunt/DTOs/Game/GameDto.cs
+++ ScavengerHunt/DTOs/Game/GameDto.cs
@@ -4,10 +4,12 @@
     {
         public Guid Id { get; set; }
         public bool IsPrivate { get; set; }
+        public bool IsReadyToPlay {get; set;}
         public string Name { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+        public Guid UserId { get; set; }
         public CoordinateDto Coordinate { get; set; }
         public int Items { get; set; }
         public string ImageName { get; set; }
